fe8ca3a012adf34d772fcfdbbefd1a694f95f379
Hemeswar reddy <22022cm006@gmail.com>
2024-07-14 15:48:47 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,30 @@
+document.body.style.backgroundColor = "red";
+document.title = "hemeswar";
+console.log("Hi I'm Hemes here")
+let a = 10;
+let b = 20.6;
+let c = "Hemeswar";
+let d = null;
+let e = undefined;
+let f = 'M';
+let g = true;
+let h= {
+    "name": "Hemeswar",
+    "age": 19, 
+    "employee id": 6
+};
+h.salary = 5000;
+console.log(h)
+console.log(a, b ,c,d,e,f,g,"Your age is " +h["employee id"]);
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f, typeof g, typeof h.age)
+// console.log("The value of a is "+ a)
+// if(a==2){
+//     console.log("The value of a is "+ a);
+// }
+// else{
+//     let a=10;
+//     console.log(
+//        "Your assumption is wrong. The value of a is "+ a 
+//     )
+// }
+// alert("This page is made with javascript")
