6e1aa619fd591042cb8f4a0552ad71cbf6daae58
Hemeswar reddy <22022cm006@gmail.com>
2024-07-31 10:14:16 +0530

    commit

--- conversionproject/script.js
+++ conversionproject/script.js
@@ -1,15 +1,35 @@
 let link = "https://2024-03-06.currency-api.pages.dev/v1/currencies/"
-let countryfrom = "usd"
-let countryto = "inr"
-function getNestedProperty(obj, path) {
-    return path.reduce((acc, key) => (acc && acc[key] !== undefined) ? acc[key] : undefined, obj);
+let countryfrom = document.querySelector("#from")
+console.log(countryfrom.value);
+let countryto = document.querySelector("#to")
+console.log(countryto.value);
+let select = document.querySelectorAll("select")
+
+for (const iterator of select) {
+    for (const key in countryList) {
+        let options = document.createElement("option")
+        options.value = countryList[key]
+        options.innerText = countryList[key]
+        iterator.append(options)
+    }
+    iterator.addEventListener("change", (evt)=>{
+        updateflag(evt.target)
+    })
 }
-let fetching = async () =>{
-    let convert = await fetch(`${link}${countryfrom}.json`);
-    let data = await convert.json();
-    console.log(data.)
+function updateflag(element){
+    let curcode = element.value;
+    let image = element.parentElement.querySelector("img")
+    image.src = `https://raw.githubusercontent.com/hampusborgos/country-flags/main/png1000px/${curcode.toLowerCase()}.png`
 }
-let getdata = async () =>{
-    await fetching();
-}
-getdata()
+
+  
+// let fetching = async () =>{
+//     let convert = await fetch(`${link}${countryfrom}.json`);
+//     let data = await convert.json();
+//     value = data
+//     console.log(value)
+// }
+// let getdata = async () =>{
+//     await fetching();
+// }
+// getdata()
