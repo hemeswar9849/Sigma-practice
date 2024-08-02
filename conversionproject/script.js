b60e1abb7d6e2666f533deadc3d8888a3011eb2a
Hemeswar reddy <22022cm006@gmail.com>
2024-08-02 17:47:32 +0530

    commit

--- conversionproject/script.js
+++ conversionproject/script.js
@@ -1,10 +1,6 @@
 let link = "https://2024-03-06.currency-api.pages.dev/v1/currencies/"
-let countryfrom = document.querySelector("#from")
-console.log(countryfrom.value);
-let countryto = document.querySelector("#to")
-console.log(countryto.value);
 let select = document.querySelectorAll("select")
-
+const button = document.querySelector("#btn")
 for (const iterator of select) {
     for (const key in countryList) {
         let options = document.createElement("option")
@@ -12,24 +8,50 @@ for (const iterator of select) {
         options.innerText = countryList[key]
         iterator.append(options)
     }
-    iterator.addEventListener("change", (evt)=>{
+    iterator.addEventListener("change", (evt) => {
         updateflag(evt.target)
     })
 }
-function updateflag(element){
+function updateflag(element) {
     let curcode = element.value;
-    let image = element.parentElement.querySelector("img")
-    image.src = `https://raw.githubusercontent.com/hampusborgos/country-flags/main/png1000px/${curcode.toLowerCase()}.png`
+    let image = element.parentElement.querySelector("img");
+    image.src = `https://raw.githubusercontent.com/hampusborgos/country-flags/main/png1000px/${curcode.toLowerCase()}.png`;
 }
+button.addEventListener("click", async () => {
+    // console.log(countryto.value);
+    // console.log(countryfrom.value);
+    let countryfrom = document.querySelector("#from").value
+    console.log(countryfrom);
+    for (const key in countryList) {
+        if (countryList[key] == countryfrom) {
+            countryfrom = key;
+        }
+    }
+    let countryto = document.querySelector("#to").value
+    console.log(countryto)
+    for (const key in countryList) {
+        if (countryList[key] == countryto) {
+            countryto = key;
+        }
+    }
+    const convert = (rate) => {
 
-  
-// let fetching = async () =>{
-//     let convert = await fetch(`${link}${countryfrom}.json`);
-//     let data = await convert.json();
-//     value = data
-//     console.log(value)
-// }
-// let getdata = async () =>{
-//     await fetching();
-// }
-// getdata()
+    }
+    let fetching = async () => {
+        let convert = await fetch(`${link}${countryfrom.toLowerCase()}.json`);
+        let data = await convert.json();
+        console.log(data)
+        const rate = data.usd[countryto]
+        console.log(rate);
+    }
+    let getdata = async () => {
+        await fetching();
+    }
+    getdata()
+
+
+})
+document.addEventListener("DOMContentLoaded", () => {
+    let from = document.querySelector("#from");
+    let usd = document.querySelector("#from option").value = 
+})
