a5035dea672fb789473b6fe6d996d3bde56a085d
Hemeswar reddy <22022cm006@gmail.com>
2024-08-03 13:21:56 +0530

    commit

--- js and Html/arrays.js
+++ js and Html/arrays.js
@@ -1,10 +1,18 @@
-let a= [1,3,4,7,2,10]
-console.log(a)
-let add = (num1, num2) =>{
-    return num1*num2;
-}
-console.log(a.reduce(add))
-console.log(Array.from("harry"))
+let uName = "My%20name%20is%20hemeswar%20reddy"
+
+console.log(uName);
+console.log(charCodeAt);
+
+// let charCodeAt = uName.charCodeAt()
+// let lowerCase = uName.toLowerCase()
+// let toUpperCase = uName.toUpperCase()
+// let replaced = uName.replace(/%20/g," ")
+// let subString = uName.substring(0,4)
+// let charAt = uName.charAt(3)
+// let trimed = uName.trimStart()
+// console.log(trimed);
+// let splited = uName.split(" ")
+// console.log(splited);
 
 
 
@@ -13,6 +21,35 @@ console.log(Array.from("harry"))
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a= [1,3,4,7,2,10]
+// console.log(a)
+// let add = (num1, num2) =>{
+//     return num1*num2;
+// }
+// console.log(a.reduce(add))
+// console.log(Array.from("harry"))
 // let greaterThan3= ((a) => {
 //     if(a>3){
 //         return true
@@ -50,6 +87,8 @@ console.log(Array.from("harry"))
 // console.log(a.join("-"))
 // a[0] = "hemes"
 // c =a.toString()
+// a.split()
+
 // console.log(typeof a)
 // console.log(typeof c)
 // let b= "hemes"
--- js and Html/tempCodeRunnerFile.js
+++ js and Html/tempCodeRunnerFile.js
@@ -1,9 +1,1 @@
-try {
-    console.log(a)
-} 
-catch (error) {
-    console.log(`You haven't declared this before`)
-}
-finally{
-    console.log("I am sorry")
-}
+😍
