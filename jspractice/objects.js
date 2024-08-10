7fd12fd4b00813544c4253599870295efbc34f09
Hemeswar reddy <22022cm006@gmail.com>
2024-08-10 11:25:47 +0530

    commit

--- jspractice/objects.js
+++ jspractice/objects.js
@@ -1,12 +1,31 @@
-let obj = {
-    userName: "hemeswar",
-    age: 19,
-    id: 6,
-    1: "two", 
-    junnu: function()  {
-        return this.userName;
-    }
-}
-let userName = "userName";
-console.log(obj.junnu());
+// let obj = {
+//     userName: "hemeswar",
+//     age: 19,
+//     id: 6,
+//     1: "two", 
+//     junnu: function()  {
+//         return this.userName;
+//     }
+// }
+// let userName = "userName";
+// console.log(obj.junnu());
+// let arr = [1,2,
+//     {
+//         userName: "hemes",
+//         age: 19,
+//         pin: 6
+//     },
+//     3,4];
+// const obj = {
+//     arr,
+//     age : 19
+// }
+// console.log(obj);
+// console.log(arr);
+// [1,2,3,4,5,6,7,8].forEach(item => (console.log(item)))
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// console.log(arr);
+
+
 
