831d0d6bb0caf663f2e7fec3058064f554873739
Hemeswar reddy <22022cm006@gmail.com>
2024-08-14 17:20:24 +0530

    commit

--- questionForToday/factorial.js
+++ questionForToday/factorial.js
@@ -3,11 +3,11 @@ let aNumber = 0;
 let temp = aNumber;
 let result = 1;
 
-if(aNumber <= 0){
+if (aNumber <= 0) {
     result = 0;
 } else {
-    for(let index = 1; index <= aNumber; index++){
-        result = index * result;    
+    for (let index = 1; index <= aNumber; index++) {
+        result = index * result;
     }
 }
 
