d90d53c82c700a6fd7ecc0ae8a2308f878fb7f77
Hemeswar reddy <22022cm006@gmail.com>
2024-10-03 06:49:05 +0530

    practiced some of the topics in nodejs

---
+++ nodejs/calc.js
@@ -0,0 +1,20 @@
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const sub = (a, b) => {
+  return a - b;
+};
+
+export const mul = (a, b) => {
+  return a * b;
+};
+
+// export default add;
+//common js
+// module.exports = {
+//   add,
+//   sub
+// };
+
+// ECMAScript Modules
---
+++ nodejs/use.js
@@ -0,0 +1,16 @@
+// const calc = require('./calc.js');
+// named import
+// import { sub } from "./calc.js";
+//default import
+// import add from "./calc.js"
+//all possible imports
+
+// import * as calc from "./calc.js";
+
+                        //     \node js path                       \script file path
+// console.log(`node js path ${process.argv[0]}  and script path ${process.argv[1]}`);
+// console.log(process.argv[2],process.argv[3]);
+// console.log(calc.sub(2, 1));
+// console.log(calc.mul(2, 1));
+
+console.error('ERROR: 404');
