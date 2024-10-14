4179a02de06d715bd02c445b9302e62eb8524f29
Hemeswar reddy <22022cm006@gmail.com>
2024-10-14 14:55:53 +0530

    practiced some more on react

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -4,16 +4,19 @@ import './App.css';
 const Hello = ({ name, studyNow }) => {
   return (
     <div class='a-student-data'>
-    <p>Hi I'm {name}</p>
-    <p>And he is studying {studyNow}</p>
+      <p>Hi I'm {name}</p>
+      <p>And he is studying {studyNow}</p>
     </div>
   );
 }
 
 function App() {
+  const students = ['hemes', 'mohan sai', 'Jagadheesh', 'manish'];
   return (
     <div className="App">
-      <Hello name={'hems'} studyNow={'diploma 3rd year'} />
+      {students.map((student, index) => {
+        return <Hello key={index} name={student} studyNow={'diploma 3rd year'} />
+      })}
     </div>
   );
 }
