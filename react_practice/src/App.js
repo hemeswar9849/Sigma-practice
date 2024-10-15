442839f61eecadc515a450d6308e695edc8a2839
Hemeswar reddy <22022cm006@gmail.com>
2024-10-15 09:16:09 +0530

    practiced adding css in react

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 
 const Hello = ({ name, studyNow }) => {
   return (
-    <div class='a-student-data'>
+    <div className='a-student-data'>
       <p>Hi I'm {name}</p>
       <p>And he is studying {studyNow}</p>
     </div>
   );
 }
 
+const style = {
+  color: 'blue',
+  'textAlign':'center'
+};
+
 function App() {
   const students = ['hemes', 'mohan sai', 'Jagadheesh', 'manish'];
   return (
-    <div className="App">
+    <div className="App" style={style}>
       {students.map((student, index) => {
         return <Hello key={index} name={student} studyNow={'diploma 3rd year'} />
       })}
